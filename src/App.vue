4b1089bc2703b866450d036637f7dbbd7af24b7c
charly2221@hotmail.fr <charly2221@hotmail.fr>
2021-05-20 23:08:04 +0200

    Ajout d'une base front pour débuter le projet

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Logo</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li class="nav-item">
+            <router-link to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/about">About</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
   <router-view/>
 </template>
 
@@ -15,16 +29,17 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+/**
+Ici on personalise le style des boutons de la navbar
+ */
+.navbar-nav li a {
+  color: #565656;
+  text-decoration: none;
+  margin-left:20px;
+  font-weight:bold;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.navbar-nav li .router-link-exact-active {
+  color: #FFF;
 }
 </style>
---
+++ src/components/Feed.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="container">
+    <h1>Votre feed</h1>
+    <div class="mb-3">
+      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Exprimez-vous"></textarea>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <p class="card-text">fsdmlkjdfskjmlsdfkljmsfdkljmfsdlkmjqdfsljksdfqkljmljksfdqlkjfdslkjdfs.</p>
+        <p>Posté par <strong>Charly Escalona</strong></p>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+
