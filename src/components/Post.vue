f315005c8c47900d0b973e508a9f67b32d373db6
Arthur <arthur.lev44@gmail.com>
2021-05-25 16:25:05 +0200

    Ajout base

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -2,7 +2,7 @@
   <div class="card">
     <div class="card-body">
       <p class="card-text">{{ content }}.</p>
-      <p>Posté par <strong>{{ author }}</strong></p>
+      <p>Posté par <router-link :to="{ name: 'user-posts', params:{ id: author_id }}">{{author_name}}</router-link></p>
     </div>
   </div>
 </template>
@@ -12,7 +12,8 @@ export default {
   name: 'Post',
   props:{
     content:String,
-    author:String,
+    author_name:String,
+    author_id:String,
   },
 }
 </script>
