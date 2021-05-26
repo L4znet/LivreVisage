5ab714aff62e7f72f39c1d1480d7b2ca0d9cdcb3
charly2221@hotmail.fr <charly2221@hotmail.fr>
2021-05-26 16:17:27 +0200

    ajout d'un try catch sur l'affichage des posts

--- src/store/feed.js
+++ src/store/feed.js
@@ -26,9 +26,30 @@ const feed = {
         async getAllPosts(context){
             const url = "https://livrevisage-506a7-default-rtdb.europe-west1.firebasedatabase.app/feed.json";
 
-            const firebaseResponse = await axios.get(url);
+            try {
+                const firebaseResponse = await axios.get(url);
 
-            context.commit('UPDATE_FEED', firebaseResponse.data);
+                context.commit('UPDATE_FEED', firebaseResponse.data);
+
+                if(firebaseResponse.statusText !== 'OK') {
+                    throw new Error("Une erreur est survenue !");
+                }
+            } catch(error) {
+                alert(error.message);
+            }
+        },
+        async triggerDelete(store, id) {
+          const url = `https://livrevisage-c44bb-default-rtdb.europe-west1.firebasedatabase.app/feed`;
+          try {
+            const response = await axios.delete(`${url}/${id}.json`);
+    
+            if(response.statusText !== 'OK') {
+              throw new Error("Une erreur est survenue !");
+            }
+            store.dispatch('getAllPosts');
+          } catch(error) {
+            alert(error.message);
+          }
         }
     },
 
