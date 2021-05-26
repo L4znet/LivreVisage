b396889f187306ff78f3d09ec23899c27786c768
charly2221@hotmail.fr <charly2221@hotmail.fr>
2021-05-26 17:32:02 +0200

    update

---
+++ src/store/users.js
@@ -0,0 +1,31 @@
+/*
+
+import axios from 'axios'
+import router from '../router'
+
+ */
+const users = {
+    namespaced: true,
+
+    state(){
+        return {
+        }
+    },
+
+    getters:{
+
+    },
+
+    mutations:{
+        UPDATE_FEED(state, payload){
+            state.feed = payload
+        }
+    },
+
+    actions:{
+
+    },
+
+}
+
+export default users;
