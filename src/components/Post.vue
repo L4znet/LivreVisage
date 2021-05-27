a5d86c4036e09c7bf5cd2edcc474bfaba4cc8b0a
charly2221@hotmail.fr <charly2221@hotmail.fr>
2021-05-27 17:25:57 +0200

    Gros fix

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,28 +1,43 @@
 <template>
-  <div class="card">
-    <div class="card-body">
-      <p class="card-text">{{ content }}.</p>
-      <p>Posté par <router-link :to="{ name: 'user-posts', params:{ id: author_id }}">{{author_name}}</router-link></p>
-      <button @click="deleteItem(id)">Delete</button>
-    </div>
+ <div class="card">
+  <div class="card-body">
+   <p class="card-text">{{ content }}.</p>
+   <p>
+    Posté par
+    <router-link :to="{ name: 'user-posts', params: { id: author_id } }">{{
+     author_name
+    }}</router-link>
+   </p>
+   <button @click="deleteItem(id)" v-if="author_id === getId">Delete</button>
+   <button @click="editItem" v-if="author_id === getId">Modifier</button>
   </div>
+ </div>
 </template>
 
 <script>
-import {mapActions} from "vuex";
+import { mapActions } from "vuex";
 
 export default {
-  name: 'Post',
-  props:{
-    content:String,
-    author_name:String,
-    author_id:String,
-    id:String
+ name: "Post",
+ props: {
+  content: String,
+  author_name: String,
+  author_id: String,
+  id: String,
+ },
+ methods: {
+  editItem() {
+   if (this.updateMode === true) {
+    this.updateItem([
+     this.id,
+     this.author_id,
+     this.author_name,
+     this.contentToChange,
+    ]);
+   }
+   this.updateMode = !this.updateMode;
   },
-  methods:{
-    ...mapActions('feed', ['deleteItem'])
-  }
-}
+  ...mapActions("feed", ["deleteItem", "updateItem", "loadConnected"]),
+ },
+};
 </script>
-
-
